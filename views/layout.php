63be7368154774debf70010f564f2bb61efccabc
Julio Cervantes <65304872+JCervantesB@users.noreply.github.com>
2023-05-12 12:35:16 -0700

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Veterinaria</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="/build/css/app.css">
 </head>
 
@@ -17,7 +16,7 @@
         </div>
     </div>
 
-    <?php 
+    <?php
         echo $script ?? '';
     ?>
 
